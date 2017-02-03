08d2be04042ee190aaff484fa840028ef96da8aa
Bruno Rodrigues da Costa <helton@ef5.com.br>
2017-02-03 19:21:51 -0200

    Testes em diversos sites

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-var seoCrawler = require('./lib/index');
+var SeoCrawler = require('./lib/index').SeoCrawler;
 
-//seoCrawler('https://www.conectcar.com','conectcar');
-//seoCrawler('https://promonestle2.ef5.com.br/promonescau','promonescau');
-seoCrawler('https://promonestle2.ef5.com.br/nestlegelato','nestlegelato');
+// var seoCrawlerNestleGelato = new SeoCrawler('nestlegelato_maiusculo_combarra');
+// seoCrawlerNestleGelato.addUrlToQueue('https://promonestle2.ef5.com.br/NestleGelato/');
+
+// var seoCrawlerNestleGelato = new SeoCrawler('nestlegelato');
+// seoCrawlerNestleGelato.addUrlToQueue('https://promonestle2.ef5.com.br/nestlegelato');
+
+// var seoCrawlerCPW = new SeoCrawler('promoCPW');
+// seoCrawlerCPW.addUrlToQueue('https://promonestle2.ef5.com.br/promonescau/');
+
+
+// var seoCrawlerNestleGelato = new SeoCrawler('promonescau_prod');
+// seoCrawlerNestleGelato.addUrlToQueue('https://www.promonestle.com.br/promonescau');
+
+// var seoCrawlerOVDF = new SeoCrawler('ovalordofeminino');
+// seoCrawlerOVDF.addUrlToQueue('http://nestle2-internet.ef5.com.br/molico/ovalordofeminino/');
+
+// var seoCrawlerOVDF = new SeoCrawler('assai');
+// seoCrawlerOVDF.addUrlToQueue('https://promonestle.local/assai/');
+
+
+var seoCrawlerOVDF = new SeoCrawler('pascoaquerende');
+seoCrawlerOVDF.addUrlToQueue('https://promonestle2.ef5.com.br/pascoaquerende');
+
+
